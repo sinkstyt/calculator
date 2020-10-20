37e766b76670a83aa786a76ed23f5ea2d4cf8863
Tyler Sinks <tyler.sinks@gmail.com>
2020-10-19 20:58:18 -0400

    change the h1 and title, add p, h3

--- js/scripts.js
+++ js/scripts.js
@@ -16,6 +16,7 @@ function divide(number1, number2) {
 
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
+// Line just below this one looks like it hard codes which operation our calc is going to do
 const result = divide(number1, number2);
 
 alert(result);
